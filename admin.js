b0cea312bc92a4641b9df1349ce2c4b224cda503
Manish-XD <manish.pgl41@gmail.com>
2022-09-16 09:59:02 +0530

    added admin.js

---
+++ admin.js
@@ -0,0 +1,11 @@
+const titleInput = document.getElementsByClassName("title");
+const linkInput = document.getElementsByClassName("link");
+const addBtn = document.getElementsByClassName("addBtn");
+
+// show function (aditi)
+
+
+// add function (tushar)
+
+
+// delete function (akshat)
